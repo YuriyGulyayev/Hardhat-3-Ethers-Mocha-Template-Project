f93443bc3629e35ca3d8637b179ac80a654e3082
Yuriy Gulyayev <yuriy.gulyayev@gmail.com>
2026-03-20 21:53:43 -0400

    Refactored contract static code analysis tools launhers. Id=202603216

--- slither/launchers/Slither.bash
+++ slither/launchers/Slither.bash
@@ -8,11 +8,12 @@ main()
    local -r slitherOutputFileName_=../Hardhat-3-Ethers-Mocha-Template-Project-Slither-Report.md
    gio trash --force -- "${slitherOutputFileName_}"
 
-   # [ToDo-202603173-1]
+   # [ToDo-202603173-2]
    # As of Mar 2026, this doesn't work because Slither doesn't support Hardhat 3.
+   # Let's hope they will add that support soon.
    # It could be necessary to pass the `--force` flag to Hardhat.
    # Revisit command line arguments.
-   # [/ToDo-202603173-1]
+   # [/ToDo-202603173-2]
    ./.venv/bin/slither --compile-custom-build 'npx hardhat build --build-profile production' --filter-paths /node_modules/ --show-ignored-findings --checklist . >> "${slitherOutputFileName_}"
 
    pw-play /usr/share/sounds/freedesktop/stereo/dialog-information.oga &
@@ -25,6 +26,8 @@ prepare()
    # set -o pipefail
    shopt -s extglob
    declare -g -r scriptFolderPath="${BASH_SOURCE[0]%%+([!/])}"
+   # shellcheck source=../../shell-script-libs/ErrorHandling.bash
+   source "${scriptFolderPath}../../shell-script-libs/ErrorHandling.bash"
 }
 
 main
