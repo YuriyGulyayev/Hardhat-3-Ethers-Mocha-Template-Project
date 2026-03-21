fc719983de28f284f361d48edda4f8a0506069d2
Yuriy Gulyayev <yuriy.gulyayev@gmail.com>
2026-03-20 21:56:51 -0400

    Refactored Ignition modules. Id=202603222

--- ignition/modules/Counter.ts
+++ ignition/modules/Counter.ts
@@ -1,16 +1,19 @@
-import type {ContractOptions} from "@nomicfoundation/ignition-core";
+// import type {ContractOptions} from "@nomicfoundation/ignition-core";
 import {buildModule} from "@nomicfoundation/hardhat-ignition/modules";
+import {IgnitionHelpers} from "../src/IgnitionHelpers.ts"
 
+// By default, transaction requests are sent on behalf of the signer at the index of zero.
 export default buildModule(
    "CounterModule",
    (ignitionModuleBuilder_) => {
-      const deployerAccountIndex_ = 1;
-      const contractOptions_: ContractOptions = {from: ignitionModuleBuilder_.getAccount(deployerAccountIndex_),};
+      IgnitionHelpers.warnIfDeploymentStateExistsOnce();
+
+      /** Comment-202603184 applies. */
       const defaultX_ = 1234n;
-      const counter_ =
-         ignitionModuleBuilder_.contract("Counter", [defaultX_,], contractOptions_);
+      
+      const counter_ = ignitionModuleBuilder_.contract("Counter", [defaultX_,]);
       const by_ = 5n;
-      ignitionModuleBuilder_.call(counter_, "incBy", [by_,], contractOptions_);
+      ignitionModuleBuilder_.call(counter_, "incBy", [by_,]);
       return {
          counter: counter_,
       };
