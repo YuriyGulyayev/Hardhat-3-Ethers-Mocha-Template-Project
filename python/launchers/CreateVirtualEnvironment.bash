2af58d70b3acf255b755ab58b9717c19222ae759
Yuriy Gulyayev <yuriy.gulyayev@gmail.com>
2026-03-20 21:52:26 -0400

    Refactored Python launchers. Id=202603215

--- python/launchers/CreateVirtualEnvironment.bash
+++ python/launchers/CreateVirtualEnvironment.bash
@@ -19,6 +19,8 @@ prepare()
    # set -o pipefail
    shopt -s extglob
    declare -g -r scriptFolderPath="${BASH_SOURCE[0]%%+([!/])}"
+   # shellcheck source=../../shell-script-libs/ErrorHandling.bash
+   source "${scriptFolderPath}../../shell-script-libs/ErrorHandling.bash"
 }
 
 main
